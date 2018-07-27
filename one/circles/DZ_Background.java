abc42cf91d4d0ed5d6cc31ec9f977e5e6fe1915a
dj_Duff <duffdj@gmail.com>
2018-07-27 15:12:46 +0300

    Commit 2

--- one/circles/DZ_Background.java
+++ one/circles/DZ_Background.java
@@ -1,6 +1,17 @@
 package one.circles;
 
+import java.awt.*;
+
 public class DZ_Background {
+
+    private final Color backgroundColor = new Color(
+            (int)(Math.random() * 255),
+            (int)(Math.random() * 255),
+            (int)(Math.random() * 255)
+    );
+
+
+    void 
 
 
 
